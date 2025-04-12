d3b0dcf9f853e7b6e193120d333946b7c714733f
Alex Wang <2214725335@qq.com>
2025-04-12 22:12:11 +0800

    简化代码

--- src/Main.java
+++ src/Main.java
@@ -5,19 +5,20 @@ import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) {
-        Stack<String> ops = new Stack<String>();
-        Stack<Double> vals = new Stack<Double>();
+        Stack<String> ops = new Stack<>();
+        Stack<Double> vals = new Stack<>();
         while (!StdIn.isEmpty()){
             String s = StdIn.readString();
-            if (s.equals("("));
-            else if (s.equals("+")) ops.push(s);
-            else if (s.equals("*")) ops.push(s);
-            else if (s.equals(")")){
-                String op = ops.pop();
-                if (op.equals("+")) vals.push(vals.pop() + vals.pop());
-                else if (op.equals("*")) vals.push(vals.pop()*vals.pop());
+            switch (s) {
+                case "(" -> {}
+                case "+", "*" -> ops.push(s);
+                case ")" -> {
+                    String op = ops.pop();
+                    if (op.equals("+")) vals.push(vals.pop() + vals.pop());
+                    else if (op.equals("*")) vals.push(vals.pop() * vals.pop());
+                }
+                default -> vals.push(Double.parseDouble(s));
             }
-            else vals.push(Double.parseDouble(s));
         }
         StdOut.println(vals.pop());
     }
