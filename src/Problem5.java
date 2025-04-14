0c45ae6794608414ee1410b42b568496289100d7
Alex Wang <2214725335@qq.com>
2025-04-14 13:37:11 +0800

    整合两个函数computePostfix, infix2postfix

--- src/Problem5.java
+++ src/Problem5.java
@@ -3,35 +3,44 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class Problem5 {
-    /**
-     * 判断是否为运算符
-     *
-     * @param c 需要判断的字符
-     */
-    private static boolean isOperator(char c) {
-        return c == '+' || c == '-' || c == '*' || c == '/';
-    }
-
-    /**
-     * 得到运算符的优先级
-     * @param op 运算符
-     * @return int 越大说明优先级越高
-     */
-    private static int getPriority(char op) {
-        return switch (op) {
-            case '+', '-' -> 1;
-            case '*', '/' -> 2;
-            default -> 0; // 括号优先级最低
-        };
-    }
-
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String s = input.nextLine();
+        String Postfix = infix2postfix(s);
+        System.out.printf("%s,%.2f",Postfix,computePostfix(Postfix));
+    }
+
+    /**
+     * 计算后缀表达式
+     * @param Postfix 后缀表达式
+     * @return 值
+     */
+    public static Double computePostfix(String Postfix){
+        Stack<Double> values = new Stack<>();
+
+        String[] elements = Postfix.split(" ");
+        for (String element : elements) {
+            switch (element) {
+                case "+" -> values.push(values.pop() + values.pop());
+                case "-" -> values.push(-(values.pop() - values.pop()));
+                case "*" -> values.push(values.pop() * values.pop());
+                case "/" -> values.push(1 / (values.pop() / values.pop()));
+                default -> values.push(Double.parseDouble(element));
+            }
+        }
+        return values.pop();
+    }
+
+    /**
+     * 把中缀表达式转为后缀表达式
+     * @param infix 中缀表达式
+     * @return 后缀表达式
+     */
+    public static String infix2postfix(String infix){
         ArrayList<String> postfix = new ArrayList<>();
         Stack<Character> ops = new Stack<>();
 
-        Scanner input = new Scanner(System.in);
-        String s = input.nextLine();
-        char[] charArray = s.toCharArray();
+        char[] charArray = infix.toCharArray();
         int N = charArray.length;
 
         for (int i = 0; i < N; i++) {
@@ -71,23 +80,28 @@ public class Problem5 {
             postfix.add(ops.pop().toString());
         }
 
-        String result = String.join(" ", postfix);
+        return String.join(" ", postfix);
+    }
 
-        // 计算表达式
-        Stack<Double> values = new Stack<>();
+    /**
+     * 判断是否为运算符
+     *
+     * @param c 需要判断的字符
+     */
+    private static boolean isOperator(char c) {
+        return c == '+' || c == '-' || c == '*' || c == '/';
+    }
 
-        String[] elements = result.split(" ");
-        for (String element : elements) {
-            switch (element) {
-                case "+" -> values.push(values.pop() + values.pop());
-                case "-" -> values.push(-(values.pop() - values.pop()));
-                case "*" -> values.push(values.pop() * values.pop());
-                case "/" -> values.push(1 / (values.pop() / values.pop()));
-                default -> values.push(Double.parseDouble(element));
-            }
-        }
-        Double value = values.pop();
-
-        System.out.printf("%s,%.2f",result,value);
+    /**
+     * 得到运算符的优先级
+     * @param op 运算符
+     * @return int 越大说明优先级越高
+     */
+    private static int getPriority(char op) {
+        return switch (op) {
+            case '+', '-' -> 1;
+            case '*', '/' -> 2;
+            default -> 0; // 括号优先级最低
+        };
     }
 }
